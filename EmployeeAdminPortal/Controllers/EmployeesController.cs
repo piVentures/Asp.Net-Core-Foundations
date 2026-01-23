edc9beaa2826e4a73ac76d64dcc3bd22d65e97b9
kira23j <kirubelmamuyesahle@gmail.com>
2026-01-23 18:18:37 +0300

    Add AddEmployeeDto and implement AddEmployee method in EmployeesController

--- EmployeeAdminPortal/Controllers/EmployeesController.cs
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models.Entities;
+using EmployeeAdminPortal.Models.Dtos;
 using System.Linq;
 
 namespace EmployeeAdminPortal.Controllers
@@ -21,5 +23,26 @@ namespace EmployeeAdminPortal.Controllers
             var allEmployees = dbContext.Employees.ToList(); // Fetch all employees
             return Ok(allEmployees); // Return 200 OK with data
         }
+
+        
+        
+        [HttpPost] // POST api/employees
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            // Map the incoming DTO to the Employee entity
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployeeDto.Name,      // Employee name from request
+                Email = addEmployeeDto.Email,    // Employee email from request
+                Phone = addEmployeeDto.Phone,    // Optional phone number
+                Salary = addEmployeeDto.Salary   // Salary
+            };
+
+            dbContext.Employees.Add(employeeEntity); // Add the new employee to the DbContext
+            dbContext.SaveChanges();                 // Persist changes to the database
+
+            return Ok(employeeEntity); // Return 200 OK with the newly added employee
+        }
+
     }
 }
