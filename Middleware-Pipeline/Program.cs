6e379e8a9eee5a82150300bc14a7e37aed024bb9
kira23j <kirubelmamuyesahle@gmail.com>
2026-02-02 12:12:54 +0300

    implement CUSTOM middleware pipeline with conditional branching

--- Middleware-Pipeline/Program.cs
+++ Middleware-Pipeline/Program.cs
@@ -1,6 +1,66 @@
+using System.Reflection.Metadata.Ecma335;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+#region Middleware - 1
+// First middleware in the pipeline
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Middleware #1: Before calling next \r\n");
 
+    await next(context);
+
+    await context.Response.WriteAsync("Middleware #1: After calling next \r\n");  
+});
+#endregion
+
+#region Conditional Middleware with UseWhen
+// Branch the pipeline when request path starts with /employees and query contains "id"
+// This only runs for requests that satisfy the condition
+app.UseWhen(context =>
+    context.Request.Path.StartsWithSegments("/employees") && context.Request.Query.ContainsKey("id"),
+    (appBuilder) =>
+{
+    // Middleware #5 inside the conditional branch
+    appBuilder.Use(async (HttpContext context, RequestDelegate next) =>
+    {
+        await context.Response.WriteAsync("Middleware #5: Before calling next \r\n");
+        await next(context);
+        await context.Response.WriteAsync("Middleware #5: After calling next \r\n");
+    });
+
+    // Middleware #6 inside the conditional branch
+    appBuilder.Use(async (HttpContext context, RequestDelegate next) =>
+    {
+        await context.Response.WriteAsync("Middleware #6: Before calling next \r\n");
+        await next(context);
+        await context.Response.WriteAsync("Middleware #6: After calling next \r\n");
+    });
+});
+#endregion
+
+#region Middleware - 2
+// Second middleware in the main pipeline
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Middleware #2: Before calling next \r\n");
+    await next(context);
+    await context.Response.WriteAsync("Middleware #2: After calling next \r\n");
+});
+#endregion
+
+#region Middleware - 3
+// Third middleware in the main pipeline
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Middleware #3: Before calling next \r\n");
+    await next(context);
+    await context.Response.WriteAsync("Middleware #3: After calling next \r\n");
+});
+#endregion
+
+#region Terminal Middleware
+// Terminal middleware that ends the pipeline
 app.Run();
+#endregion
